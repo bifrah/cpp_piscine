43c6036f0dd758f3a4b422eaa4f7284d191813e6
Benjamin <benj.ifrah@gmail.com>
2022-08-21 12:16:35 +0200

    save

---
+++ cpp07/ex02/Array.hpp
@@ -0,0 +1,71 @@
+#ifndef ITER_HPP
+# define ITER_HPP
+
+#include <iostream>
+#include <exception>
+
+template <typename T>
+class Array {
+
+public:
+
+	/*------Constructor and Destructor------*/
+	Array(void) : _size(0), _array(new T[0]()) {}
+	Array(unsigned int size) : _size(size), _array(new T[size]()) {}
+	Array(const Array &original_array) : _size(original_array._size), _array(new T[_size]())
+	{
+		*this = original_array;
+	}
+	~Array(void)
+	{
+		delete [] _array;
+	}
+
+	/*---------------Overload---------------*/
+	Array &operator=(const Array &arr)
+	{
+		unsigned int i = 0;
+
+		if (_array != NULL)
+			delete [] _array;
+		_size = arr._size;
+		_array = new T[_size];
+		while (i < _size)
+		{
+			_array[i] = arr._array[i];
+			i++;
+		}
+		return (*this);
+	}
+
+	T &operator[](unsigned int i)
+	{
+		if (i >= _size)
+			throw(SegFault());
+		else
+			return (_array[i]);
+	}
+
+	/*--------------Exceptions--------------*/
+	class SegFault : public std::exception
+	{
+		virtual const char *what() const throw()
+		{
+			return("\nBad index.\n");
+		}
+	};
+
+	/*-----------Member functions-----------*/
+	unsigned int size(void)
+	{
+		return (_size);
+	}
+
+private:
+
+	unsigned int	_size;
+	T				*_array;
+
+};
+
+#endif
---
+++ cpp07/ex02/main.cpp
@@ -0,0 +1,74 @@
+#include "Array.hpp"
+#include <cstring>
+
+
+template <typename T>
+void print_array(T &array, unsigned int size)
+{
+	unsigned int i = 0;
+
+	while (i < size)
+	{
+		try
+		{
+			std::cout << array[i] << " ";
+		}
+		catch (std::exception &e)
+		{
+			std::cout << e.what() << " ";
+		}
+		i++;
+	}
+	std::cout << std::endl;
+}
+
+int	main(void)
+{
+	std::cout << "\n----------Test with empty array : ----------" << std::endl;
+	Array <int>test0(void);
+
+	std::cout << "\n----------Test with int : ------------------" << std::endl;
+	Array <int>test(42);
+
+	std::cout << "\n----------Default values : -----------------" << std::endl;
+	print_array(test, 42);
+
+	std::cout << "\n----------Set values : ---------------------" << std::endl;
+	test[2] = -42;
+	test[21] = 42;
+	print_array(test, 42);
+
+	std::cout << "\n----------Access beyond bounds : -----------" << std::endl;
+	try
+	{
+		std::cout << test[45];
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	try
+	{
+		test[43] = 999;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	print_array(test, 45);
+
+	std::cout << "\n----------Copy test 1 : --------------------" << std::endl;
+	Array <int>test2(3);
+	test2 = test;
+	print_array(test2, 42);
+
+	std::cout << "\n----------Copy test 2 : --------------------" << std::endl;
+	Array <int>test3(test2);
+	print_array(test3, 42);
+
+	std::cout << "\n----------Test with std::string : ----------" << std::endl;
+	Array <std::string>test4(42);
+	test4[2] = "HELLO";
+	test4[21] = "WORLD";
+	print_array(test4, 42);
+}
