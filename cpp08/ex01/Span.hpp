869d6666f81e379bc537c74f83a9ee560b04ac46
bifrah <benj.ifrah@gmail.com>
2022-08-23 17:22:03 +0200

    passage en template

--- cpp08/ex01/Span.hpp
+++ cpp08/ex01/Span.hpp
@@ -4,17 +4,40 @@
 #include <iostream>
 #include <vector>
 
+template <typename T>
 class Span {
 
 public:
 
-	Span(unsigned int N);
-	Span(const Span &span);
-	~Span();
+	Span::Span(unsigned int N) : _N(N)
+	{
+		std::cout << "Span default constructor called." << std::endl;
+	}
 
-	Span&	operator=(const Span &span);
+	Span::Span(const Span &span) 
+	{
+		*this = span;
+		std::cout << "Span copy constructor called." << std::endl;
+	}
 
-	unsigned int	getN(void) const ;
+	Span::~Span()
+	{
+		std::cout << "Span destructor called." << std::endl;
+	}
+
+	Span&	operator=(const Span &span)
+	{
+		if (this == &span)
+			return (*this);
+		this->_N = span.getN();
+		return (*this);
+		std::cout << "Span assignation copy constructor called" << std::endl;
+	}
+
+	unsigned int	getN(void) const
+	{
+		return (this->_N);
+	}
 
 private:
 
