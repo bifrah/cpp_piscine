3b32c99832267fc1adf89947520753f85317a5ae
Benjamin <benj.ifrah@gmail.com>
2023-04-25 07:11:21 +0200

    add exit before core dumped

--- cpp09/ex01/RPN.hpp
+++ cpp09/ex01/RPN.hpp
@@ -10,26 +10,46 @@
 class OperandsumException : public std::runtime_error {
 public:
 	OperandsumException() : std::runtime_error("Error: not enough operands for + operator") {}
+	virtual const char* what() const throw() {
+		std::cerr << std::runtime_error::what() << std::endl;
+		exit(128);
+	}
 };
 
 class OperandsubException : public std::runtime_error {
 public:
 	OperandsubException() : std::runtime_error("Error: not enough operands for - operator") {}
+	virtual const char* what() const throw() {
+		std::cerr << std::runtime_error::what() << std::endl;
+		exit(128);
+	}
 };
 
 class OperandmulException : public std::runtime_error {
 public:
 	OperandmulException() : std::runtime_error("Error: not enough operands for * operator") {}
+	virtual const char* what() const throw() {
+		std::cerr << std::runtime_error::what() << std::endl;
+		exit(128);
+	}
 };
 
 class OperanddivException : public std::runtime_error {
 public:
 	OperanddivException() : std::runtime_error("Error: not enough operands for / operator") {}
+	virtual const char* what() const throw() {
+		std::cerr << std::runtime_error::what() << std::endl;
+		exit(128);
+	}
 };
 
 class DivideByZeroException : public std::runtime_error {
 public:
 	DivideByZeroException() : std::runtime_error("Error: division by zero") {}
+	virtual const char* what() const throw() {
+		std::cerr << std::runtime_error::what() << std::endl;
+		exit(128);
+	}
 };
 
 class RPN {
