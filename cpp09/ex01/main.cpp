e6530566341b87573ce25ba0fe70360f148b7e9d
bifrah_home <benj.ifrah@gmail.com>
2023-05-13 17:14:10 +0200

    better conversion

--- cpp09/ex01/main.cpp
+++ cpp09/ex01/main.cpp
@@ -1,33 +1,44 @@
 #include "RPN.hpp"
 
-int main(int argc, char** argv) {
-	RPN calc;
-	for (int i = 1; i < argc; i++)
+int main(int argc, char** argv)
+{
+    RPN calc;
+    for (int i = 1; i < argc; i++)
 	{
-		std::stringstream ss(argv[i]);
-		std::string token;
-		while (getline(ss, token, ' '))
+        std::stringstream ss(argv[i]);
+        std::string token;
+        while (getline(ss, token, ' '))
 		{
-			if (!token.empty())
+            if (!token.empty())
 			{
-				if (token == "+")
-					calc.add();
-				else if (token == "-")
-					calc.sub();
-				else if (token == "*")
-					calc.mul();
-				else if (token == "/")
-					calc.div();
-				else 
-					calc.push(atof(token.c_str()));
-			}
-		}
-	}
-	try {
-		std::cout << calc.result() << std::endl;
-	} catch (const std::exception& ex) {
-		std::cerr << ex.what() << std::endl;
-		return (1);
-	}
-	return 0;
+                if (token == "+")
+                    calc.add();
+                else if (token == "-")
+                    calc.sub();
+                else if (token == "*")
+                    calc.mul();
+                else if (token == "/")
+                    calc.div();
+                else
+				{
+                    std::stringstream converter(token);
+                    float value;
+                    if (converter >> value)
+                        calc.push(value);
+                    else
+					{
+                        std::cerr << "Invalid token: " << token << std::endl;
+                        return 1;
+                    }
+                }
+            }
+        }
+    }
+    try {
+        std::cout << calc.result() << std::endl;
+    } catch (const std::exception& ex) {
+        std::cerr << ex.what() << std::endl;
+        return (1);
+    }
+    return 0;
 }
